86e7bb44eae270b8239633f07c16583d227cd1ab
Yevhenii Hyzyla <hyzyla@gmail.com>
2021-06-20 22:58:34 +0300

    Add allowed host config

--- kato/settings.py
+++ kato/settings.py
@@ -27,10 +27,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DJANGO_DEBUG'] == 'true'
 
-ALLOWED_HOSTS = [
-    '0.0.0.0',
-    'localhost'
-]
+ALLOWED_HOSTS = os.environ['DJANGO_ALLOWED_HOSTS'].split(',')
 
 
 # Application definition
